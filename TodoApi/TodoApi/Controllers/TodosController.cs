17fa00d08c9efcec1cf2b692a84b6b7fbb413b6a
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-21 13:12:29 +0200

    Defined the controllers

---
+++ TodoApi/TodoApi/Controllers/TodosController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models.Todos;
+
+namespace TodoApi.Controllers
+{
+    [Route("[controller]")]
+    public class TodosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTodos()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateTodo([FromRoute] int id, [FromBody] TodoTask task)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult CreateTodo(TodoTask task)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteTodo([FromRoute] int id)
+        {
+            return NoContent();
+        }
+    }
+}
