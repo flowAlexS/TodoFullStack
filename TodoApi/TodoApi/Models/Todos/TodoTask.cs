c1fa2acb70d3928d2230b134ffeef1518c21cda1
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-21 13:09:56 +0200

    Added the TodoTask Model

---
+++ TodoApi/TodoApi/Models/Todos/TodoTask.cs
@@ -0,0 +1,29 @@
+﻿namespace TodoApi.Models.Todos
+{
+    public class TodoTask
+    {
+        public Guid Id { get; }
+
+        public string Title { get; }
+
+        public string Note { get; }
+
+        public bool Completed { get; }
+
+        public int Order { get; }
+
+        // Enforce here...
+        public TodoTask(
+            Guid id,
+            string title,
+            string note,
+            int order)
+        {
+            Id = id;
+            Title = title;
+            Note = note;
+            Completed = false;
+            Order = order;
+        }
+    }
+}
